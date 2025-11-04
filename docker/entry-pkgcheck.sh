dfde1991b284349aefa5cb5e33e43e08fe8d8a3f
Simon Urbanek <simon.urbanek@R-project.org>
2025-11-04 15:08:54 +1300

    use CRAN scripts for checks

--- docker/entry-pkgcheck.sh
+++ docker/entry-pkgcheck.sh
@@ -24,5 +24,65 @@ if [ ! -e /pkg ]; then
     fi
 fi
 
-cd /pkg
-exec /build/bin/Rscript -e 'chooseCRANmirror(graphics=FALSE, ind=1, local.only=TRUE); tools::check_packages_in_dir("/pkg")'
+## the check scripts rely on the structure where QA is the home
+ln -sfn /src/QA/bin ~/bin
+ln -sfn /src/QA/lib ~/lib
+## the work space is ~/tmp/CRAN
+if [ ! -e /build/CRAN ]; then
+    mkdir /build/CRAN
+fi
+if [ ! -e ~/tmp ]; then
+    mkdir ~/tmp
+fi
+ln -sfn /build/CRAN ~/tmp/CRAN
+
+## handle .R
+mkdir -p ~/.R
+cp /src/QA/.R/* ~/.R/
+
+## repos relies on local copies so check if they are mounted, otherwise replace with online versions
+if [ ! -e /data/Repositories ]; then
+    echo NOTE: local /data/Repositories are not mounted, switching to online versions
+    if [ -z "${CRAN_MIRROR}" ]; then CRAN_MIRROR=https://cloud.r-project.org; fi
+    ## there is a lot of stuff in the regular Rprofile that is local, so we replace it
+    ## with a smaller version - FIXME: it would be nice to decouple the local and global parts
+    echo "local({ utils::setRepositories(FALSE,1:4); r=getOption('repos'); r[1]='${CRAN_MIRROR}'; options(repos=r) })" > ~/.R/Rprofile
+    ## we also need a site version of this
+    if [ ! -e /build/etc/Rprofile.site ]; then
+	echo "local({ utils::setRepositories(FALSE,1:4); r=getOption('repos'); r[1]='${CRAN_MIRROR}'; options(repos=r) })" > /build/etc/Rprofile.site
+    fi
+    ## the last part of Rprofile in QA
+    cat << 'EOF' >> ~/.R/Rprofile
+options(showErrorCalls = TRUE,
+        showWarnCalls = TRUE,
+        warn = 1)
+
+## When moving towards avoiding partial matching && friends:
+options(warnPartialMatchArgs = TRUE,
+        warnPartialMatchAttr = TRUE,
+        warnPartialMatchDollar = TRUE)
+
+## Ensure CRAN versions where available.
+options(available_packages_filters =
+            c("R_version", "OS_type", "subarch", "CRAN", "duplicates"))
+EOF
+fi
+
+## R is in ~/tmp/R
+ln -sfn /build ~/tmp/R
+
+## copy packages (like getIncoming)
+cp -p /pkg/*.tar.gz ~/tmp/CRAN/
+ls -l ~/tmp/CRAN/
+
+export PATH=$HOME/bin:$PATH
+
+cd ~/tmp/CRAN
+mkdir -p Library
+
+## FIXME: tools suggest xml2, curl and others which are required for the checks,
+## but they are not auto-installed. So until that is fixed, we have to manually
+## install those
+R_LIBS=$HOME/tmp/CRAN/Library MAKEFLAGS=-j6 /build/bin/Rscript -e 'p=c("curl","xml2"); i=p[!p %in% rownames(installed.packages())]; if(length(i)) install.packages(i)'
+
+check_CRAN_incoming -n "$@"
